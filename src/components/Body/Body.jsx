484b6557d375824b1a33dad2e927ce5adc518a28
Deepak Kumar Karn <93374385+dhuruvkarn@users.noreply.github.com>
2023-02-16 02:37:36 +0530

    pie chart

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Body.css";
+import Chart from "react-apexcharts";
 import LeftUpper from "../Left/Upper/LeftUpper";
 import LeftLower from "../Left/Lower/LeftLower";
 import RightUpper from "../Right/Upper/RightUpper";
 import RightLower from "../Right/Lower/RightLower";
 function Body() {
+  const options = {
+    series: [53, 48],
+    labels: ["Female", "Male"],
+    colors: ["#ff9c66", "#ff5902"],
+  };
   return (
     <>
       <div className="bodyMainDiv">
@@ -15,7 +21,14 @@ function Body() {
         </div>
 
         {/* centre */}
-        <div className="bodyCentreDiv"></div>
+        <div className="bodyCentreDiv">
+          <Chart
+            options={options}
+            series={options.series}
+            type="pie"
+            width="100%"
+          />
+        </div>
 
         {/* left */}
         <div className="bodyLeftDiv">
